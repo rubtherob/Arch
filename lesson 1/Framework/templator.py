84341c186661dc512be7ea51dc2a52de8d1072c2
rubtherob <rubtherobs@gmail.com>
2022-08-17 18:21:03 +0300

    Lesson 3

--- lesson 1/Framework/templator.py
+++ lesson 1/Framework/templator.py
@@ -1,9 +1,9 @@
-from jinja2 import Template
-import os
+from jinja2 import Template, FileSystemLoader
+from jinja2.environment import Environment
 
 
 def render(template_name, folder='templates', **kwargs):
-    file_path = os.path.join(folder, template_name)
-    with open(file_path, encoding='utf-8') as f:
-        template = Template(f.read())
+    env = Environment()
+    env.loader = FileSystemLoader(folder)
+    template = env.get_template(template_name)
     return template.render(**kwargs)
